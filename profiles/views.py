31012508cfc7c686578343b36652a4763aef6ca8
tahmid-saj <tahmid.saj@gmail.com>
2023-09-02 01:52:36 -0400

    Storing uploaded file

--- profiles/views.py
+++ profiles/views.py
@@ -5,10 +5,16 @@ from django.http import HttpResponseRedirect
 # Create your views here.
 
 
+def store_file(file):
+    with open("temp/test.txt", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
+
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        print(request.FILES["image"])
+        store_file(request.FILES["image"])
         return HttpResponseRedirect("/profiles")
