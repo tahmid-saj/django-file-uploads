02621764beaf19c5cf5c63cf84a084c44777386e
tahmid-saj <tahmid.saj@gmail.com>
2023-09-02 13:58:43 -0400

    Storing review id

--- reviews/views.py
+++ reviews/views.py
@@ -47,6 +47,7 @@ class AddFavoriteView(View):
     def post(self, request):
         review_id = request.POST["review_id"]
         fav_review = Review.objects.get(pk=review_id)
-        request.session["favorite_review"] = fav_review
+        request.session["favorite_review"] = review_id
         
         return HttpResponseRedirect("/reviews/" + review_id)
+
