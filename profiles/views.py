38abb1b89b32de1cbc463dc66d06536ded6c3e1b
tahmid-saj <tahmid.saj@gmail.com>
2023-09-02 02:13:53 -0400

    using models for file storage

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.http import HttpResponseRedirect
 
 from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
@@ -24,7 +25,8 @@ class CreateProfileView(View):
         submitted_form = ProfileForm(request.POST, request.FILES)
 
         if submitted_form.is_valid():
-            store_file(request.FILES["image"])
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
             return HttpResponseRedirect("/profiles")
 
         return render(request, "profiles/create_profile.html", {
